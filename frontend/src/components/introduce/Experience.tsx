af9a212a3fc46d580a3a7d3e32771a2144a94dbb
FreshSangchoo <aprldpdy289@naver.com>
2025-06-07 01:19:54 +0900

    [feat] Create Introduction Experience

--- frontend/src/components/introduce/Experience.tsx
+++ frontend/src/components/introduce/Experience.tsx
@@ -1,12 +1,70 @@
+import "@/css/introduce/Experience.css";
+
 function Experience() {
   return (
-    <div>
-      경력사항들
-      <div>EXPERIENCE</div>
-      <div>
-        <div>
-          <div>SSAFY</div>
-          <div>2024.01. ~ 2024.12.</div>
+    <div className="experience-container">
+      <div className="title-container-text">EXPERIENCE</div>
+      <div className="experience-card-container">
+        <div className="experience-card">
+          <div className="experience-contents">
+            <div className="experience-title">SSAFY</div>
+            <div className="experience-period">2024.1. ~ 2024.12.</div>
+          </div>
+          <div className="experience-description">
+            삼성 청년 SW 아카데미에서 1년 동안 집중적인 교육과 프로젝트를
+            수행하며 개발 실무 역량을 키웠습니다. <br />
+            1학기에는 Java와 Vue를 기반으로 한 웹 개발을 중심으로 학습했으며,
+            객체지향 설계와 MVC 구조, 데이터베이스 처리 등 기본기와 협업 방식에
+            대한 이해를 쌓았습니다. 2학기에는 React를 활용한 팝업스토어 서비스,
+            React Native를 활용한 금융 통합 앱, Kotlin을 활용한 청각장애인을
+            위한 앱을 개발했습니다. <br />
+            프로젝트에서는 팀장과 프론트엔드 개발을 맡아 전체 UI/UX 설계와
+            사용자 흐름을 주도했습니다. 기술뿐만 아니라 협업, 기획, 발표에
+            이르기까지 프로젝트 전반을 리드하며 개발자로서의 종합적인 역량을
+            쌓았습니다.
+          </div>
+        </div>
+        <div className="experience-card">
+          <div className="experience-contents">
+            <div className="experience-title">
+              서강대학교
+              <br />
+              총학생회장
+            </div>
+            <div className="experience-period">2023.4. ~ 2023.12.</div>
+          </div>
+          <div className="experience-description">
+            2023년 서강대학교 총학생회장으로서 학교와 학우들 간의 가교가 되어
+            다양한 요구와 민원을 수렴하고, 이를 학교 본부와의 협의를 통해
+            제도적으로 반영하는 과정을 주도했습니다. <br />
+            총학생회 운영은 단순한 대외활동이 아닌, 리더십·커뮤니케이션·문제
+            해결 역량을 종합적으로 요구하는 역할이었고, 이를 통해 팀을 이끄는
+            책임감과 조율 능력을 키울 수 있었습니다. 이 경험은 이후 팀
+            프로젝트에서도 구성원들의 의견을 균형 있게 반영하고, 프로젝트의
+            방향을 설득력 있게 정리하는 데 큰 도움이 되었습니다.
+          </div>
+        </div>
+        <div className="experience-card">
+          <div className="experience-contents">
+            <div className="experience-title">
+              QANDA
+              <br />
+              Contents Team 인턴
+            </div>
+            <div className="experience-period">2022.9. ~ 2023.2.</div>
+          </div>
+          <div className="experience-description">
+            QANDA에서 Contents Team 인턴으로 근무하며, 수학 문제에 대한 해설과
+            컷들을 제작하고 편집하는 업무를 담당했습니다. 업무 과정에서 실제
+            앱에 게시된 콘텐츠가 어떻게 사용자에게 보여지는지를 자주 확인하게
+            되었고, 자연스럽게 앱의 UI/UX 흐름에 관심을 갖게 되었습니다.
+            <br />
+            근무를 하면서 개발팀과 협업하는 과정에서 회의에 참관하거나 개발 진행
+            상황을 지켜보며, 사용자에게 더 나은 경험을 제공하기 위한 노력에 깊은
+            인상을 받았습니다.
+            <br />이 경험을 통해 직접 사용자에게 전달되는 서비스를 개발하고
+            싶다는 꿈을 더욱 키우게 되었습니다.
+          </div>
         </div>
       </div>
     </div>
